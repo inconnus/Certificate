336a4bf23544f04f0fe0bf575b7c01004e136337
Arsapol <im.arsapol@gmail.com>
2021-09-19 00:54:46 +0700

    add apis

---
+++ lib/DynamoDB/index.d.ts
@@ -0,0 +1,53 @@
+export type PROJECT_KEYS = Array<string>
+
+export type QUERY = {
+  tableName: string
+  pk: string
+  pv: any
+  limit?: number
+  project?: PROJECT_KEYS | []
+}
+
+export type QUERY_INDEX_BETWEEN = {
+  tableName: string
+  indexName: string
+  pk: string
+  pv: string
+  sk: string
+  start: string | number
+  end: string | number
+  project?: PROJECT_KEYS
+  limit?: number
+}
+
+export type QUERY_INDEX_SORT = {
+  tableName: string
+  indexName: string
+  pk: string
+  pv: string
+  sk: string
+  sv: string
+  project?: PROJECT_KEYS
+  limit?: number
+}
+
+export type QUERY_SORT = {
+  tableName: string
+  pk: string
+  pv: string
+  sk: string
+  sv: string
+  project?: PROJECT_KEYS | []
+  limit?: number
+}
+
+export type QUERY_BETWEEN = {
+  tableName: string
+  pk: string
+  pv: string
+  sk: string
+  start: string | number
+  end: string | number
+  project?: PROJECT_KEYS | []
+  limit?: number
+}
--- pages/api/getPresignedUrl/index.ts
+++ pages/api/getPresignedUrl/index.ts
@@ -23,12 +23,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         key: id,
         ACL: "public-read",
       },
-      // Conditions: [
-      //   ["starts-with", "$Content-Type", `image/`],
-      //   ["content-length-range", 0, 1000000],
-      // ],
       Expires: 30,
-      // Bucket: path ? `${root_path}/${path}` : `${root_path}/crm-images/products/${id}/${nanoid()}`
       Bucket: `${root_path}/${id}.${ext}`
     }, (err, response) => {
       if (err) {
@@ -36,8 +31,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         console.log(err)
         return res.status(500).json(signed)
       }
-      // console.log({ err, response })
-      // console.log({ Key: response.fields.key })
       signed = response
     })
   }
